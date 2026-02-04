f544a72bbf6f9b3a38f78b458d4ce6cafc5270d2
Alexander <pchp250g8@tutamail.com>
2026-02-04 18:02:01 +0300

    Modified source code

--- DrawEllipse/Form1.cs
+++ DrawEllipse/Form1.cs
@@ -10,15 +10,21 @@ namespace DrawEllipse
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+            Rectangle rcPaint = new Rectangle(0,0, ClientSize.Width, ClientSize.Height);
+            Pen oRedPen = new Pen(Color.Red, 5);
             g.Clear(Color.Black);
-            g.DrawEllipse(Pens.Yellow, 0, 0, ClientSize.Width, ClientSize.Height);
-            g.FillEllipse(Brushes.Red, 0, 0, ClientSize.Width, ClientSize.Height);
-
+            g.DrawEllipse(oRedPen,rcPaint);
+            g.FillEllipse(Brushes.Yellow, rcPaint);
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
             Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
